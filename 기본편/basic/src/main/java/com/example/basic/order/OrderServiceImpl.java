a30efbc2fe603f07243cf63df14faa016c3cb2da
chaerlo127 <monynony0203@gmail.com>
2023-06-20 15:50:38 +0900

    core basic session 3. 새로운 할인 정책 적용과 문제점

--- 기본편/basic/src/main/java/com/example/basic/order/OrderServiceImpl.java
+++ 기본편/basic/src/main/java/com/example/basic/order/OrderServiceImpl.java
@@ -2,14 +2,14 @@ package com.example.basic.order;
 
 
 import com.example.basic.discount.DiscountPolicy;
-import com.example.basic.discount.FixDiscountPolicy;
 import com.example.basic.member.Member;
 import com.example.basic.member.MemberRepository;
 import com.example.basic.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
