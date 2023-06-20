206df2de14a9d2685bdf9d72e41b8d27ae096c8d
chaerlo127 <monynony0203@gmail.com>
2023-06-20 14:21:14 +0900

    core basic session 2. 도메인 회원 실행과 테스트

--- 기본편/basic/src/main/java/com/example/basic/member/MemberServiceImpl.java
+++ 기본편/basic/src/main/java/com/example/basic/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.basic.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 인터페이스가 이닌 구현체를 의존하기 때문에 DIP, OCP 에 위배 됨.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
