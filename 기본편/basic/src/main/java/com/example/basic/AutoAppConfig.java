de38cd00ee286c7c5cbea92e9a227c49f0ce7130
chaerlo127 <monynony0203@gmail.com>
2023-06-28 01:55:09 +0900

    core basic session 6. 탐색 위치와 기본 스캔 대상

--- 기본편/basic/src/main/java/com/example/basic/AutoAppConfig.java
+++ 기본편/basic/src/main/java/com/example/basic/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan // @Component 찾아서 스프링 빈으로 다 등록을 해줌
         (
+                basePackages = "com.example.basic",
+                basePackageClasses = AutoAppConfig.class,
                 excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 그 중에서 뺄 것을 지정해줌.
         )
 public class AutoAppConfig {
