a1bde48e85a6264beea949a58ab673517f4d0c2e
chaerlo127 <monynony0203@gmail.com>
2023-06-28 17:56:34 +0900

    core basic session 7. 옵션 처리

---
+++ 기본편/basic/src/test/java/com/example/basic/autowired/AutowiredTest.java
@@ -0,0 +1,42 @@
+package com.example.basic.autowired;
+
+import com.example.basic.member.Member;
+import jakarta.annotation.Nullable;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Optional;
+
+public class AutowiredTest {
+    @Test
+    void AutowiredOption(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+
+    static class TestBean{
+
+        // 호출 자체가 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        // null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        //Optional.empty 호출
+        @Autowired
+        public void setNoBean2(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+
+
+
+}
