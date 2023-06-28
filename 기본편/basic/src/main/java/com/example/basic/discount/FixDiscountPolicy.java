45a16cac7c0922eb2d3bddb28b66d9223531aab1
chaerlo127 <monynony0203@gmail.com>
2023-06-28 20:59:36 +0900

    core basic session 7. 조회 빈이 2개 이상 - 문제

--- 기본편/basic/src/main/java/com/example/basic/discount/FixDiscountPolicy.java
+++ 기본편/basic/src/main/java/com/example/basic/discount/FixDiscountPolicy.java
@@ -3,6 +3,7 @@ package com.example.basic.discount;
 import com.example.basic.member.Grade;
 import com.example.basic.member.Member;
 
+//@Component -> 조회 빈이 2개 이상 - 문제 에러 발생 같은 역할의 두개의 빈이 생성되기 때문
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000;
     @Override
