40888b454d1be5f6470dcbf1dff481378ee4973b
chaerlo127 <monynony0203@gmail.com>
2023-07-12 18:39:42 +0900

    core basic session 8. 빈 생명 주기 콜 백 시작

---
+++ 기본편/basic/src/test/java/com/example/basic/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package com.example.basic.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        // 부모는 자식을 담을 수 있음.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        ac.close(); // ApplicationContext -> ConfigurableApplicationContext 로 변경하여 사용한 이유
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("www.chaeng-ni.com");
+            return networkClient;
+        }
+    }
+}
