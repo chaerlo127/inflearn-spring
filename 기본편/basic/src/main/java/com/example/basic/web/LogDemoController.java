e37fd3145e32edebff99453faacb95d12a206876
chaerlo127 <monynony0203@gmail.com>
2023-07-16 23:36:53 +0900

    #1 core basic session 9. ObjectProvider 를 이용하여 웹 request 스코프 생성

--- 기본편/basic/src/main/java/com/example/basic/web/LogDemoController.java
+++ 기본편/basic/src/main/java/com/example/basic/web/LogDemoController.java
@@ -3,6 +3,7 @@ package com.example.basic.web;
 import com.example.basic.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest servletRequest){
         String url = servletRequest.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestUrl(url);
 
         myLogger.log("controller test");
--- 기본편/basic/src/main/java/com/example/basic/web/LogDemoService.java
+++ 기본편/basic/src/main/java/com/example/basic/web/LogDemoService.java
@@ -2,14 +2,16 @@ package com.example.basic.web;
 
 import com.example.basic.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("serviceID = " + id);
     }
 }
