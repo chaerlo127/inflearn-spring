eb3cbbabd82a965bca391b7a92583c8046ef00a8
chaerlo127 <monynony0203@gmail.com>
2023-08-25 15:18:18 +0900

    #10 mvc1 section1: hello servlet

--- MVC1/servlet/src/main/java/spring/mvc1/servlet/ServletApplication.java
+++ MVC1/servlet/src/main/java/spring/mvc1/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package spring.mvc1.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 스프링이 자동으로 서블릿을 등록하여 사용할 수 있도록 해줌
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ MVC1/servlet/src/main/java/spring/mvc1/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package spring.mvc1.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+// name과 urlPatterns는 다른 서블릿과 겹치면 안됨
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
