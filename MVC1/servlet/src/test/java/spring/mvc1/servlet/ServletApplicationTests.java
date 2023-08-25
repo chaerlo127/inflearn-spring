94ce1edaed32426aa330feb0bbb22d148744e964
chaerlo127 <monynony0203@gmail.com>
2023-08-25 14:51:12 +0900

    #10 create servlet basic project

---
+++ MVC1/servlet/src/test/java/spring/mvc1/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mvc1.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
